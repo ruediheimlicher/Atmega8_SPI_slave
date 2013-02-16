6185ed549c79deac7e4c5df9fd9b370e58cc97cb
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2013-02-16 12:17:27 +0100

    Defines in defs.h ausgelagert

--- Atmega8_SPI_slave.c
+++ Atmega8_SPI_slave.c
@@ -16,6 +16,7 @@
 #include <inttypes.h>
 //#define F_CPU 4000000UL  // 4 MHz
 #include <avr/delay.h>
+#include "defs.h"
 #include "lcd.c"
 #include "adc.c"
 
@@ -33,58 +34,11 @@ uint16_t loopCount2=0;
 #define GRUPPE_DS		0xC0
 //#define GRUPPE_DS	0xB0
 
-#define TWI_PORT		PORTC
-#define TWI_PIN		PINC
-#define TWI_DDR		DDRC
 
-#define SDAPIN		4
-#define SCLPIN		5
-
-#define SPI_PORT     PORTB
-#define SPI_PIN      PINB
-#define SPI_DDR      DDRB
-#define SPI_MOSI     3  
-#define SPI_MISO     4
-#define SPI_SCK      5
-
-#define LOOPLED_PORT	PORTD
-#define LOOPLED_DDR	DDRD
-#define LOOPLED_PIN	4
-
-#define TASTE1		19
-#define TASTE2		29
-#define TASTE3		44
-#define TASTE4		67
-#define TASTE5		94
-#define TASTE6		122
-#define TASTE7		155
-#define TASTE8		186
-#define TASTE9		205
-#define TASTEL		223
-#define TASTE0		236
-#define TASTER		248
-#define TASTATURPORT PORTC
-#define TASTATURPIN		3
-
-#define MANUELL_PORT		PORTD
-#define MANUELL_DDR		DDRD
-#define MANUELL_PIN		PIND
-
-#define MANUELL			7	// Bit 7 von Status 
-#define MANUELLPIN		6	// Pin 6 von PORT D fuer Anzeige Manuell
-#define MANUELLNEU		7	// Pin 7 von Status. Gesetzt wenn neue Schalterposition eingestellt
-#define MANUELLTIMEOUT	100 // Loopled-counts bis Manuell zurueckgesetzt wird. 02FF: ca. 100 s
 
 
 #define FOSC 1000000    /* oscillator-frequency in Hz */
 #define BAUD 57600  //valid values:9600, 19200, 57600 kbits
-
-
-
-
-
-
-
 
 volatile uint8_t					Programmstatus=0x00;
 	uint8_t Tastenwert=0;
@@ -101,6 +55,9 @@ volatile uint8_t spicount=0;
 size_t inpos=0;
 volatile uint8_t outpos=0;
 volatile uint8_t textpos=0;
+
+volatile uint8_t data_array[8];
+volatile uint8_t arraypos=0;
 
 
 //volatile char text[] = {'A','B','C','D','E','F','G','H'};
